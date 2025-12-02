f520750d5c8fa6142f5359e3ea59dae62e3b164a
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-02 19:57:41 +0300

    HelloWorld

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -16,5 +16,15 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello,World!!!", "Hello");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
